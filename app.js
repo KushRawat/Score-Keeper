a3f0ac40dafb8b5243e1a45b99058cc7ca9c8f80
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2021-04-29 14:54:21 +0530

    adding score selector,reset function, style

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const p1Button = document.querySelector('#p1Button')
 const p2Button = document.querySelector('#p2Button')
-
+const resetButton = document.querySelector('#reset')
 const p1Display = document.querySelector('#p1Display')
 const p2Display = document.querySelector('#p2Display')
+const winningScoreSelect = document.querySelector('#plays')
 
 let p1Score = 0
 let p2Score = 0
-let winningScore = 5
+let winningScore = 3
 let isGameOver = false
 
 p1Button.addEventListener('click', function (e) {
@@ -14,6 +15,8 @@ p1Button.addEventListener('click', function (e) {
         p1Score++
         if (p1Score === winningScore) {
             isGameOver = true
+            p1Display.classList.add('winner')
+            p2Display.classList.add('loser')
         }
         p1Display.textContent = p1Score
     }
@@ -25,7 +28,27 @@ p2Button.addEventListener('click', function (e) {
         p2Score++
         if (p2Score === winningScore) {
             isGameOver = true
+            p2Display.classList.add('winner')
+            p1Display.classList.add('loser')
         }
         p2Display.textContent = p2Score
     }
 })
+
+function reset() {
+    isGameOver = false
+    p1Score = 0
+    p2Score = 0
+    p1Display.textContent = 0
+    p2Display.textContent = 0
+    p1Display.classList.remove('winner','loser')
+    p2Display.classList.remove('winner','loser')
+}
+
+resetButton.addEventListener('click', reset) 
+
+
+winningScoreSelect.addEventListener('change', function(e) {
+    winningScore = parseInt(this.value)
+    reset()
+})  
